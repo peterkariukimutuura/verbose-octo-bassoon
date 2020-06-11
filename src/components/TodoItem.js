dbb75c55fdb249389b91ea8181d8820e048e7dec
peterkariukimutuura <kariukipeter30@gmail.com>
2020-06-12 01:47:01 +0300

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+class TodoItem extends Component {
+
+    getStyle = () => {
+        return {
+            backgroundColor: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }
+    }
+
+
+
+    render() {
+        const {id,title}  = this.props.todo;
+
+        return (
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this,id)}/>
+                    {title}
+                </p>
+            </div>
+        )
+    }
+
+}
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+export default TodoItem;
+
+
