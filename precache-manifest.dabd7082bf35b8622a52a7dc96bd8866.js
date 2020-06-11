2716f74356a92aac248a68c3384bb1af3be38784
peterkariukimutuura <kariukipeter30@gmail.com>
2020-06-12 02:11:28 +0300

    Updates

---
+++ precache-manifest.dabd7082bf35b8622a52a7dc96bd8866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56130b35aeb80fb73b205ea1fb14187",
+    "url": "/verbose-octo-bassoon/index.html"
+  },
+  {
+    "revision": "29df841a59be9b50a3a7",
+    "url": "/verbose-octo-bassoon/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c4236174b61a25951732",
+    "url": "/verbose-octo-bassoon/static/js/2.0b1e0712.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/verbose-octo-bassoon/static/js/2.0b1e0712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29df841a59be9b50a3a7",
+    "url": "/verbose-octo-bassoon/static/js/main.76b878db.chunk.js"
+  },
+  {
+    "revision": "cec68016d45126c22840",
+    "url": "/verbose-octo-bassoon/static/js/runtime-main.86976311.js"
+  }
+]);
